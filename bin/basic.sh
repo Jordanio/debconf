e7b4dc99898b2f63e29494ceb1bfc3171f822b88
pierre-lecocq <pierre.lecocq@gmail.com>
2013-01-08 10:51:06 +0100

    Update bin/basic.sh

--- bin/basic.sh
+++ bin/basic.sh
@@ -10,7 +10,7 @@ if [[ $CHOICE == "y" ]]; then
 
     apt-get update
     apt-get autoremove --purge -y exim4-.\* portmap rpcbind
-    apt-get install -y git subversion curl irb rsync emacs emacs-goodies-el php-elisp nmap fail2ban httperf tree unzip build-essential python task ruby libruby ruby-dev rubygems libmysql-ruby libactiverecord-ruby
+    apt-get install -y git subversion curl irb rsync emacs emacs-goodies-el php-elisp nmap fail2ban httperf tree unzip gcc build-essential linux-headers-$(uname -r) python task ruby libruby ruby-dev rubygems libmysql-ruby libactiverecord-ruby
 
     echo
     echo "Install bash and emacs basic config ? (y/N) "
