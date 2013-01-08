a23e0343f48e736ca91a3801447c1d92dec6f411
Pierre Lecocq <pierre.lecocq@gmail.com>
2013-01-08 19:49:40 +0100

    Some X fixes

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -15,7 +15,7 @@ if [[ $CHOICE == "y" ]]; then
 
         apt-get update
         apt-get install -y xorg openbox obconf obmenu tint2 xcompmgr feh rxvt-unicode
-        apt-get install -y lxappearance tango-icon-theme conky grun gksu i3lock ttf-bitstream-vera libnotify-bin notification-daemon
+        apt-get install -y lxappearance tango-icon-theme conky grun gksu suckless-tools ttf-bitstream-vera libnotify-bin notification-daemon
         apt-get install -y alsa alsa-tools alsa-oss alsamixergui
         apt-get install -y chromium-browser cmus vlc
 
