ed57baf9e3473d356946489fd6e228101ae0bb8b
Pierre Lecocq <pierre.lecocq@gmail.com>
2013-01-08 12:27:09 +0100

    Fix bg autostart

--- src/dot/config/openbox/autostart.sh
+++ src/dot/config/openbox/autostart.sh
@@ -1,7 +1,7 @@
 xset b 0 0 &
 synclient tapButton1=1 &
 xcompmgr -fF -I-.001 -O-.002 -D3 &
-feh --bg-scale ~/images/wallpaper.png
+feh --bg-scale ~/images/debian-blue.png &
 conky &
 tint2 &
 rxvt &
