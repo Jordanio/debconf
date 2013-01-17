a255247ca0473c56766b51cce057d6208cdd10ce
pierre <pierre@laptaupe.example.com>
2013-01-17 21:48:31 +0100

    orage

--- bin/desktop.sh
+++ bin/desktop.sh
@@ -18,7 +18,7 @@ if [[ $CHOICE == "y" ]]; then
 
         apt-get update
         apt-get install -y xorg openbox obconf obmenu tint2 xcompmgr feh rxvt-unicode
-        apt-get install -y lxappearance tango-icon-theme conky grun gksu suckless-tools ttf-bitstream-vera furiusisomount thunar
+        apt-get install -y lxappearance tango-icon-theme conky grun gksu suckless-tools ttf-bitstream-vera furiusisomount thunar orage
         apt-get install -y libnotify-bin notification-daemon
         apt-get install -y alsa alsa-tools alsa-oss alsamixergui
         apt-get install -y iceweasel icedove cmus vlc scrot pidgin xpdf mirage
