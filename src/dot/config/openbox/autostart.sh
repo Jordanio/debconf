f5413a2f833cee3933f8ffdf9e0feddb1ef4a822
pierre <pierre@laptaupe.example.com>
2013-02-09 12:16:39 +0100

    Conky

--- src/dot/config/openbox/autostart.sh
+++ src/dot/config/openbox/autostart.sh
@@ -3,7 +3,7 @@ xscreensaver -nosplash &
 synclient tapButton1=1 &
 xcompmgr -fF -I-.001 -O-.002 -D3 &
 feh --bg-scale ~/Images/frenchtouch.jpg &
-conky -c ~/.conky/conkyrc_grey &
+conky -c ~/.config/conkyrc &
 tint2 &
 rxvt &
 emacs &
